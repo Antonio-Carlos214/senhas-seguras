164bd684e2a68e26ac739c31326418cb4dd30087
Antonio-Carlos214 <antonio.zamilian@escola.pr.gov.br>
2026-07-02 08:50:36 -0300

    bhgyv7r6

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
  const numerosenha = Document.queryselector('.parametro-senha__texto');
 
  numerosenha.textcontent = 5;
+
+ const botoes = document.querySelector('.parametro-senha__botao');
+
+console.log(botoes);
